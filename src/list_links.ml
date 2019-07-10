ca9749c95d6a7c676d18953173fe2be3ea0b7817
Eduardo Santamaria <eduardo@onna.com>
2019-07-10 10:32:31 +0200

    Initial commit with oasis file, makefile and a little example

---
+++ src/list_links.ml
@@ -0,0 +1,38 @@
+(* This file is in the public domain *)
+
+(** Connect to https://ocaml.org/index.fr.html and download all the png images
+   of the page in /tmp *)
+
+open Mechaml
+module M = Agent.Monad
+open M.Infix
+open Printf
+
+type state = Ok of string | Error of string * exn
+
+
+let dummy_print to_print = 
+  printf "%s\n" to_print;;
+
+
+
+
+
+let list_links source max_depth = 
+  Agent.get source
+  >|= (fun response ->
+    response
+    |> Agent.HttpResponse.page
+    |> Page.links
+    |> Page.to_list
+  )
+
+let _ =
+  list_links "https://www.onna.com" 1
+  |> M.run (Agent.init ())
+  |> snd
+  |> List.iter (fun url -> 
+    url
+    |> Page.Link.href
+    |> dummy_print
+  )
